97fb961e19ad6dd6fa9ef8ed71377c233689fa69
Asad <contact@asadbek.dev>
2023-12-23 14:58:53 -0500

    ts depreaction error suppressed

--- lib/uploadfile.ts
+++ lib/uploadfile.ts
@@ -11,8 +11,10 @@ export async function uploadFile(file: File, isPfp?: boolean): Promise<UploadFil
     const formData = new FormData();
     formData.append('file', file);
 
+    const serverURL =  process.env.NODE_ENV === "development" ? 'http://127.0.0.1:3002' : 'https://server.mylinx.cc' 
+
     //@ts-ignore
-    const upload = await fetch(`http://127.0.0.1:3002/upload/image`, {
+    const upload = await fetch(`${serverURL}/upload/image`, {
       method: 'POST',
       body: formData as unknown as URLSearchParams, 
     });
